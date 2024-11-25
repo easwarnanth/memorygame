381c2f205facbca6da437157e4a008cab6bec8e8
easwarnanth <easwarnanthu0@gmail.com>
2024-11-25 15:37:23 +0545

    flipgame

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html','/js/*.js'], // This includes all HTML files in the project
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
